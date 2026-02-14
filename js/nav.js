14783d48fc3647d1ab9a62f443b40d80fa3c0a39
nvu1991 <nvu111991@gmail.com>
2026-02-14 08:08:08 +0700

    Edit nav to absolute path

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,6 @@
 // nav.js - Insert navigation bar with automatic path handling
 
 (function() {
-  // Calculate relative path to root based on current page depth
-  function getBasePath() {
-    const path = window.location.pathname;
-    const depth = (path.match(/\//g) || []).length - 1;
-    
-    if (depth <= 0) return './';
-    return '../'.repeat(depth);
-  }
-
   function insertNav() {
     const base = getBasePath();
     
@@ -17,15 +8,15 @@
     nav.className = 'article-nav';
     
     nav.innerHTML = `
-      <a href="${base}index.html" class="nav-brand">
-        <img src="${base}images/Kan Aikido Logo_clearWhite_80x80.webp" alt="Kan Aikido">
+      <a href="/" class="nav-brand">
+        <img src="/images/Kan Aikido Logo_clearWhite_80x80.webp" alt="Kan Aikido">
         Kan Aikido
       </a>
       <div class="nav-links">
-        <a href="${base}about/">Giới thiệu</a>
-        <a href="${base}index.html#practice">Luyện tập</a>
-        <a href="${base}index.html#join">Tham gia</a>
-        <a href="${base}index.html#contact">Liên hệ</a>
+        <a href="/about/">Giới thiệu</a>
+        <a href="/#practice">Luyện tập</a>
+        <a href="/#join">Tham gia</a>
+        <a href="/#contact">Liên hệ</a>
       </div>
     `;
     

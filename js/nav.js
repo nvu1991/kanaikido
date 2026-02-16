d04e4e82d9123c75bbf2d16f279a2ce3077de99d
nvu1991 <nvu111991@gmail.com>
2026-02-16 08:25:23 +0700

    Restore text

--- js/nav.js
+++ js/nav.js
@@ -9,6 +9,7 @@
     nav.innerHTML = `
       <a href="/" class="nav-brand">
         <img src="/images/Kan Aikido Logo_clearWhite_80x80.webp" alt="Kan Aikido">
+        Kan Aikido
       </a>
       <div class="nav-links">
         <a href="/about/">Giới thiệu</a>
